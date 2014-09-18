6e8d98209f06ed61b05233dc1a969e5731bf6ab0
Matt <mattmogit@gmail.com>
2014-09-17 17:45:30 -0700

    final submission for assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+## https://class.coursera.org/rprog-007 assignment #2
+## Introducing the <<- operator to cache data. In this case the example
+## caches the result of solve(), which is compute intensive.
 
-## Write a short comment describing this function
+## These are teh get and set functions for the matrix and inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setmatrix <- function(solve) m <<- solve
+        getmatrix <- function() m
+        list(set = set, 
+             get = get, 
+             setmatrix = setmatrix, 
+             getmatrix = getmatrix)
 }
 
 
-## Write a short comment describing this function
+## This function returns a matrix that is the inverse of x. It checks
+## if m is not null and returns the cached data. If m is null, then 
+## it calls solve() on the matrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        m <- x$getmatrix()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setmatrix(m)
+        m
 }
